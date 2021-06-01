18ced0eb2b62f9fc127d971abfa41319aedc93df
Kirill Churikov <63352010+Wh1ter@users.noreply.github.com>
2021-06-01 20:23:41 +0300

    ready to build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import appActions from "./redux/app/app-actions";
+import { connect } from "react-redux";
+
+class App extends Component {filterArr = (fArr) => {
+  let newArr = fArr.filter((cur) =>
+    cur.name.toUpperCase().includes(this.props.filter)
+  );
+  return newArr;
+};
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmitData={this.props.formSubmitHandler} />
+      <h1>Contacts</h1>
+      <Filter setFilterToState={this.props.filterSet} />
+      <ContactList
+        contacts={this.filterArr(this.props.contacts)}
+        del={this.props.contactDelete}
+      />
     </div>
   );
 }
+}
+const mapStateToProps = (state) => ({
+contacts: state.app.contacts,
+filter: state.app.filter,
+});
+const mapDispatchToProrps = (dispatch) => ({
+formSubmitHandler: (contactData) =>
+  dispatch(appActions.addContact(contactData)),
+contactDelete: (contactId) => dispatch(appActions.deleteContact(contactId)),
+filterSet: (str) => dispatch(appActions.filterSet(str)),
+});
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProrps)(App);
